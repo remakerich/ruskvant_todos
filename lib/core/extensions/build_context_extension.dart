b25ccf68777a38c98377ec8d281772cec8b92ac5
Константин Поповских <remakerja@gmail.com>
2024-02-20 13:24:46 +0600

    build

--- lib/core/extensions/build_context_extension.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ruskvant_todos/core/core.dart';
-
-extension BuildContextExtension on BuildContext {
-  NavigatorState get navigator => Navigator.of(this);
-}
--- lib/core/extensions/navigator_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ruskvant_todos/core/core.dart';
-
-extension NavigatorStateExtension on NavigatorState {
-  void go(Widget screen) => push(
-        MaterialPageRoute(
-          builder: (context) => screen,
-        ),
-      );
-}
